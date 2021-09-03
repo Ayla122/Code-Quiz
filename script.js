8d378c456df3dd8dbfdc7b022f45ded3316fcc34
Ayla122 <ayladd122@gmail.com>
2021-09-03 13:46:24 -0700

    button function

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ var score = 0;
 var questionIndex = 0;
 //questions
 var questions = [
-     {
+    {
         title: "A very useful tool for used during development and debugging for printing content to the debugger is:",
         choices: ["Javascript", "terminal / bash", "for loops", "console log"],
         answer: "console log"
@@ -33,3 +33,27 @@ var questions = [
     },
 ];
 
+//start code
+var currentTime = document.querySelector("#currentTime");
+var timer = document.querySelector("#startTime")
+var quesstionsDiv = document.querySelector("#questionsDiv")
+var wrapper = document.querySelector("#wrapper")
+var timeLeft = 80;
+var penalty = 16;
+var holdInterval = 0;
+var ulCreate = document.createElement("ul");
+
+timer.addEventListener("onClick", function() {
+    if (holdInterval === 0) {
+        holdInterval = setInterval(function() {
+            timeLeft--;
+            currentTime.textContent = "Time " = timeLeft;
+
+            if (timeLeft <= 0) {
+                clearInterval(holdInterval);
+                allDone();
+                currentTime.textContent = "Time's up!";
+            }
+        }, 1000);
+    } render(questionIndex);
+});

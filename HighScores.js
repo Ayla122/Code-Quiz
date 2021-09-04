e5feeb34f6200d17048c2bebc756ab1b0e85fdb8
Ayla122 <ayladd122@gmail.com>
2021-09-03 20:18:57 -0700

    clear scores

--- HighScores.js
+++ HighScores.js
@@ -8,14 +8,14 @@ goBack.addEventListener("click", function () {
 });
 var allScores = localStorage.getItem("allScores");
 allScores = JSON.parse(allScores);
-
 if (allScores !== null) {
-
     for (var i = 0; i < allScores.length; i++) {
-
         var createLi = document.createElement("li");
         createLi.textContent = allScores[i].initials + " " + allScores[i].score;
         highScore.appendChild(createLi);
-
     }
 }
+clear.addEventListener("click", function () {
+    localStorage.clear();
+    location.reload();
+});

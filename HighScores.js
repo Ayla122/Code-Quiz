e07bb6fad9ccccffe3e019cd770a47fc1e9bde16
Ayla122 <ayladd122@gmail.com>
2021-09-03 20:16:42 -0700

    scores

--- HighScores.js
+++ HighScores.js
@@ -6,3 +6,16 @@ var goBack = document.querySelector("#goBack");
 goBack.addEventListener("click", function () {
     window.location.replace("./index.html");
 });
+var allScores = localStorage.getItem("allScores");
+allScores = JSON.parse(allScores);
+
+if (allScores !== null) {
+
+    for (var i = 0; i < allScores.length; i++) {
+
+        var createLi = document.createElement("li");
+        createLi.textContent = allScores[i].initials + " " + allScores[i].score;
+        highScore.appendChild(createLi);
+
+    }
+}

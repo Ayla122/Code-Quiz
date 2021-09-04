f80dc5e2a792375562f3ee176d92f04ecada4c56
Ayla122 <ayladd122@gmail.com>
2021-09-03 20:13:08 -0700

    'go back' button

---
+++ HighScores.js
@@ -0,0 +1,8 @@
+// variables
+var highScore = document.querySelector("#highScore");
+var clear = document.querySelector("#clear");
+var goBack = document.querySelector("#goBack");
+
+goBack.addEventListener("click", function () {
+    window.location.replace("./index.html");
+});
